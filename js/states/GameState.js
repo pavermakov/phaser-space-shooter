0f5523abc3c2a6841e9881edd2af899f5640ed9e
Pavel Ermakov <pavermakov@gmail.com>
2017-02-11 13:01:55 +0300

    Added multiple levels

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -1,26 +1,28 @@
 var SpaceHipster = SpaceHipster || {};
 
 SpaceHipster.GameState = {
-  init: function() {
+  init: function () {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.PLAYER_SPEED = 200;
-    this.BULLET_SPEED = - 1000;
+    this.BULLET_SPEED = - 500;
   },
 
-  preload: function() {
+  preload: function () {
     this.load.image('space', 'assets/images/space.png');
     this.load.image('player', 'assets/images/player.png');
     this.load.image('bullet', 'assets/images/bullet.png');
     this.load.image('enemyParticle', 'assets/images/enemyParticle.png');
-    this.load.image('purpleEnemy', 'assets/images/purpleEnemy.png');
-    this.load.image('greenEnemy', 'assets/images/greenEnemy.png');
 
     this.load.spritesheet('redEnemy', 'assets/images/redEnemySpritesheet.png', 45, 45, 4);
+    this.load.spritesheet('greenEnemy', 'assets/images/greenEnemySpritesheet.png', 45, 45, 4);
+    this.load.spritesheet('purpleEnemy', 'assets/images/purpleEnemySpritesheet.png', 45, 45, 4);
+
+    this.load.text('level1', 'assets/data/level1.json');
   },
 
-  create: function() {
+  create: function () {
     this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
     this.background.autoScroll(0, 30);
 
@@ -35,14 +37,16 @@ SpaceHipster.GameState = {
 
     // enemies
     this._initEnemies();
+
+    this._loadLevel();
   },
 
-  update: function() {
+  update: function () {
     this.game.physics.arcade.overlap(this.playerBullets, this.enemies, this._damageEnemy, null, this);
     this.game.physics.arcade.collide(this.enemyBullets, this.player, this._killPlayer, null, this);
     this.player.body.velocity.x = 0;
 
-    if(this.game.input.activePointer.isDown) {
+    if (this.game.input.activePointer.isDown) {
       var targetX = this.game.input.activePointer.position.x;
       var direction = targetX > this.game.world.centerX ? 1 : -1;
 
@@ -50,30 +54,24 @@ SpaceHipster.GameState = {
     }
   },
 
-  _initEnemies: function() {
+  _initEnemies: function () {
     this.enemies = this.game.add.group();
     this.enemies.enableBody = true;
 
     this.enemyBullets = this.game.add.group();
     this.enemyBullets.enableBody = true;
-
-    var enemy = new SpaceHipster.Enemy(this.game, 100, 100, 'redEnemy', 10, this.enemyBullets);
-    this.enemies.add(enemy);
-
-    enemy.body.velocity.x = 100;
-    enemy.body.velocity.y = 50;
   },
 
-  _initBullets: function() {
+  _initBullets: function () {
     this.playerBullets = this.game.add.group();
     this.playerBullets.enableBody = true;
-    this.shootingTimer = this.game.time.events.loop(Phaser.Timer.SECOND / 5, this._createPlayerBullet, this);
+    this.shootingTimer = this.game.time.events.loop(Phaser.Timer.SECOND / 3, this._createPlayerBullet, this);
   },
 
-  _createPlayerBullet: function() {
+  _createPlayerBullet: function () {
     var bullet = this.playerBullets.getFirstExists(false);
 
-    if(!bullet) {
+    if (!bullet) {
       bullet = new SpaceHipster.PlayerBullet(this.game, this.player.x, this.player.top);
       this.playerBullets.add(bullet);
     } else {
@@ -85,13 +83,55 @@ SpaceHipster.GameState = {
     bullet.body.velocity.y = this.BULLET_SPEED;
   },
 
-  _damageEnemy: function(bullet, enemy) {
+  _createEnemy: function (x, y, health, key, scale, speedX, speedY) {
+    console.log(speedY)
+    var enemy = this.enemies.getFirstExists(false);
+
+    if (!enemy) {
+      enemy = new SpaceHipster.Enemy(this.game, x, y, key, health, this.enemyBullets);
+    }
+
+    this.enemies.add(enemy);
+    enemy.reset(x, y, health, key, scale, speedX, speedY);
+  },
+
+  _damageEnemy: function (bullet, enemy) {
     enemy.damage(1);
     bullet.kill();
   },
 
-  _killPlayer: function() {
+  _killPlayer: function () {
     this.player.kill();
     this.game.state.start('GameState');
+  },
+
+  _loadLevel: function () {
+    this.currentEnemyIndex = 0;
+    this.levelData = JSON.parse(this.game.cache.getText('level1'));
+
+    this._scheduleNextEnemy();
+  },
+
+  _scheduleNextEnemy: function () {
+    var nextEnemy = this.levelData.enemies[this.currentEnemyIndex];
+
+    if(nextEnemy) {
+      var nextTime = nextEnemy.time - (this.currentEnemyIndex === 0 ? 0 : this.levelData.enemies[this.currentEnemyIndex - 1].time);
+      nextTime *= 1000;
+
+      var x = nextEnemy.x * this.game.world.width;
+      var y = -100;
+      var health = nextEnemy.health;
+      var key = nextEnemy.key;
+      var scale = nextEnemy.scale;
+      var speedX = nextEnemy.speedX;
+      var speedY = nextEnemy.speedY;
+
+      this.nextEnemyTimer = this.game.time.events.add(nextTime, function() {
+        this._createEnemy(x, y, health, key, scale, speedX, speedY);
+        this.currentEnemyIndex++;
+        this._scheduleNextEnemy();
+      }, this);
+    }
   }
 }
